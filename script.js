d831b2fd6a34a167b3339dd74b5e279b81529d9f
ogbeche77 <ogbeche77@yahoo.com>
2020-02-07 15:13:05 +0100

    celcius

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ async function getData() { //named async bcos its an asynchronous function makin
    //parse the csv; meaning breaking up the data and arranging it objects etc
     //we use the split function here to put the element in an array
    
-    //we can split the table with line break since each row is demacated by a line break
+    //we can split the table with line break since each row is demacated by a "\n"
+    //table could also be demacated with \, \. etc
     const table = data.split("\n");
     //row refers to the rows in the table and 
     //first 3 rows are splited in column
